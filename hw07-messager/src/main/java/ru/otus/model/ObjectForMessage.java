e7ba9710a3a1da190045f9c115d51c4c19e0c1e2
AidanMegabyte <aidan.megabyte@gmail.com>
2022-03-08 23:53:23 +0300

    TODO 1, 2, 4

--- hw07-messager/src/main/java/ru/otus/model/ObjectForMessage.java
+++ hw07-messager/src/main/java/ru/otus/model/ObjectForMessage.java
@@ -1,15 +1,38 @@
 package ru.otus.model;
 
+import java.util.Collections;
 import java.util.List;
 
 public class ObjectForMessage {
+
     private List<String> data;
 
+    public ObjectForMessage() {
+        this.data = Collections.emptyList();
+    }
+
+    public ObjectForMessage(ObjectForMessage other) {
+        if (other == null) {
+            throw new IllegalArgumentException("Object for message cannot be null!");
+        }
+        setData(other.getData());
+    }
+
     public List<String> getData() {
-        return data;
+        return List.copyOf(data);
     }
 
     public void setData(List<String> data) {
-        this.data = data;
+        if (data == null) {
+            throw new IllegalArgumentException("Data cannot be null!");
+        }
+        this.data = List.copyOf(data);
+    }
+
+    @Override
+    public String toString() {
+        return "ObjectForMessage{" +
+                "data=" + data +
+                '}';
     }
 }
