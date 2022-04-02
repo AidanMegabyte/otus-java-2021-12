56e5e3703a88014f7d033d64d40d017d8ee11150
AidanMegabyte <aidan.megabyte@gmail.com>
2022-04-02 19:18:50 +0300

    Добавил сущности Address и Phone

---
+++ hw10-jpql/src/main/java/ru/otus/crm/model/Phone.java
@@ -0,0 +1,59 @@
+package ru.otus.crm.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "phone")
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "phone_generator")
+    @SequenceGenerator(name = "phone_generator", sequenceName = "phone_sequence", allocationSize = 1)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "number")
+    private String number;
+
+    public Phone() {
+    }
+
+    public Phone(String number) {
+        this.id = null;
+        this.number = number;
+    }
+
+    public Phone(Long id, String number) {
+        this.id = id;
+        this.number = number;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "id=" + id +
+                ", number='" + number + '\'' +
+                '}';
+    }
+
+    @Override
+    protected Object clone() {
+        return new Phone(this.id, this.number);
+    }
+}
--- hw10-jpql/src/main/resources/db/migration/V1__initial_schema.sql
+++ hw10-jpql/src/main/resources/db/migration/V1__initial_schema.sql
@@ -16,3 +16,19 @@ create table client
     id   bigint not null primary key,
     name varchar(50)
 );
+
+create sequence address_sequence start with 1 increment by 1;
+
+create table address
+(
+    id     bigint not null primary key,
+    street varchar(50)
+);
+
+create sequence phone_sequence start with 1 increment by 1;
+
+create table phone
+(
+    id     bigint not null primary key,
+    number varchar(12)
+);
