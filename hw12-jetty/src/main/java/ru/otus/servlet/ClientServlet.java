c99ed348410e1771780be4cc82cdd622edc41a1c
AidanMegabyte <aidan.megabyte@gmail.com>
2022-04-25 04:11:04 +0300

    Настроил веб-сервер

---
+++ hw12-jetty/src/main/java/ru/otus/servlet/ClientServlet.java
@@ -0,0 +1,71 @@
+package ru.otus.servlet;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.eclipse.jetty.util.StringUtil;
+import ru.otus.crm.model.Address;
+import ru.otus.crm.model.Client;
+import ru.otus.crm.model.Phone;
+import ru.otus.crm.service.DBServiceClient;
+import ru.otus.service.TemplateProcessor;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+
+import static org.eclipse.jetty.http.MimeTypes.Type.TEXT_HTML;
+
+public class ClientServlet extends HttpServlet {
+
+    private static final String CLIENTS_PAGE_TEMPLATE = "clients.ftl";
+
+    private final DBServiceClient clientService;
+
+    private final TemplateProcessor templateProcessor;
+
+    private final String adminRoleName;
+
+    public ClientServlet(DBServiceClient clientService, TemplateProcessor templateProcessor, String adminRoleName) {
+        this.clientService = clientService;
+        this.templateProcessor = templateProcessor;
+        this.adminRoleName = adminRoleName;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setContentType(TEXT_HTML.asString());
+
+        var data = new HashMap<String, Object>();
+        data.put("isAdmin", request.isUserInRole(adminRoleName));
+        data.put("clients", clientService.findAll());
+
+        response.getWriter().println(templateProcessor.getPage(CLIENTS_PAGE_TEMPLATE, data));
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setContentType(TEXT_HTML.asString());
+
+        if (request.isUserInRole(adminRoleName)) {
+
+            var name = request.getParameter("name");
+            var street = request.getParameter("address");
+            var phones = Arrays.stream(request.getParameterMap().get("phones"))
+                    .filter(phone -> !StringUtil.isBlank(phone))
+                    .toList();
+
+            var client = new Client(
+                    null,
+                    name,
+                    new Address(null, street),
+                    phones.stream().map(phone -> new Phone(null, phone)).toList()
+            );
+            clientService.saveClient(client);
+        }
+
+        response.sendRedirect("/clients");
+    }
+}
